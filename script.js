72798a3e0dbe8206b05981d41d701d53dd483739
eyesoulate <orane.mcmayo@gmail.com>
2020-01-28 11:57:35 -0500

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+<h1>Orane McMayo's First Website</h1>
+
+<style>
+	.p1 {
+		color: red
+	}
+</style>
+
+<p class="p1">  
+    My name is Orane McMayo and I'm addicted to coding.
+</p>
+
+<p>currently watching...</p>
+<img src="movie.jpg"/>
